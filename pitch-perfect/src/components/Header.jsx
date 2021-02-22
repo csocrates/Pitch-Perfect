0c33d32f01d9aa2cd07f551e8df56e13b28cbb42
Tomasz <tomaszruttt@gmail.com>
2021-02-22 10:50:51 +0000

    organize folder structure

---
+++ pitch-perfect/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = ({profilePicture = "https://1734811051.rsc.cdn77.org/data/images/full/372134/attn-cat-lovers-scientists-claim-using-profile-pics-of-your-and-your-cat-wont-give-you-a-date.jpg?w=600?w=650"}) => {
+    return (
+        <main className="App__Header">
+            <h1 className='App__HeaderTitle'>Pitch Perfect</h1>
+            <img className="App__HeaderProfilePic" src={profilePicture} alt="profile picture"></img>
+        </main>
+    );
+};
+
+export default Header;
