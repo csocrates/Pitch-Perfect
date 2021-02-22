02be57ea46f57841f8b9f70acab16a01355d345b
csocrates <calebsocrates@gmail.com>
2021-02-22 12:26:53 +0000

    creates campsite list component

---
+++ pitch-perfect/src/components/CampsiteList.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class CampsiteList extends Component {
+  state = { isLoading: true, campsiteList: [] };
+
+  componentDidMount() {
+    this.fetchCampsitesByLocation(this.props.map);
+  }
+  render() {
+    const { isLoading, campsiteList } = this.state;
+    //map function here which invokes campsite card component
+
+    return <div>{isLoading ? "" : campsiteList}</div>;
+  }
+  fetchCampsitesByLocation(map) {
+    let campsiteList = [];
+    const request = {
+      location: map.center,
+      query: "campsites",
+      radius: "500",
+      fields: ["name", "geometry"],
+    };
+    const service = new window.google.maps.places.PlacesService(map);
+
+    service.textSearch(request, (results, status) => {
+      if (status == window.google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 0; i < results.length; i++) {
+          campsiteList.push(results[i]);
+        }
+      }
+    });
+    this.setState({ isLoading: false, campsiteList });
+  }
+}
+
+export default CampsiteList;
