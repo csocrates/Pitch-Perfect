c5b04fb8e4a9d31e38df69548ccf005f7a391435
Dong93-dev <dong.zhou93@outlook.com>
2021-02-25 15:18:56 +0000

    added error handle

--- src/components/singleCampsitePage/SingleCampsiteInfo.jsx
+++ src/components/singleCampsitePage/SingleCampsiteInfo.jsx
@@ -13,29 +13,39 @@ class SingleCampsiteInfo extends Component {
     reviews: [],
     location: {},
     name: "",
+    googleAPIError: "",
   };
 
   componentDidMount() {
     const service = new window.google.maps.places.PlacesService(this.props.map);
-    service.getDetails({ placeId: this.props.place_id }, (place, status) => {
-      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        this.setState({
-          isLoading: false,
-          formatted_address: place.formatted_address,
-          business_status: place.business_status,
-          formatted_phone_number: place.formatted_phone_number,
-          rating: place.rating,
-          photos: place.photos,
-          reviews: place.reviews,
-          location: place.geometry.location,
-          name: place.name,
-        });
-      }
-    });
+
+    if (!this.props.map) {
+      this.setState({ isLoading: false, googleAPIError: "No map was found" });
+    } else {
+      service.getDetails({ placeId: this.props.place_id }, (place, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          this.setState({
+            isLoading: false,
+            formatted_address: place.formatted_address,
+            business_status: place.business_status,
+            formatted_phone_number: place.formatted_phone_number,
+            rating: place.rating,
+            photos: place.photos,
+            reviews: place.reviews,
+            location: place.geometry.location,
+            name: place.name,
+            googleAPIError: "",
+          });
+        } else {
+          this.setState({ isLoading: false, googleAPIError: status });
+        }
+      });
+    }
   }
 
   render() {
     if (this.state.isLoading) return "Loading";
+    if (this.state.googleAPIError) return <p>{this.state.googleAPIError}</p>;
     return (
       <main className="singleCampsitePage__singleCampsiteInfo">
         <section className="singleCampsiteInfo__singleCampsiteIntro">
