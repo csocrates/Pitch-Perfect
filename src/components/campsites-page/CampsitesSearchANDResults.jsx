3eed3a65e87bcde00e27bd52fc80fabf6c78cf39
Tomasz <tomaszruttt@gmail.com>
2021-03-02 14:43:52 +0000

    adjust a  tall screen

--- src/components/campsites-page/CampsitesSearchANDResults.jsx
+++ src/components/campsites-page/CampsitesSearchANDResults.jsx
@@ -19,7 +19,6 @@ class CampsitesSearchANDResults extends Component {
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log("navigate", position, this.state.isLoading);
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
 
